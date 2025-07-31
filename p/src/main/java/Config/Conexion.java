c0cb67591001fe81852155fd6e9e10702c160f7d
jairog <jampaex12@gmail.com>
2025-07-31 01:51:02 -0500

    Se agrega el proyecto

--- p/src/main/java/Config/Conexion.java
+++ p/src/main/java/Config/Conexion.java
@@ -10,9 +10,12 @@ public class Conexion {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vacukidsv?useSSL=false", "root", "admin1234");
         } catch (Exception e) {
             System.err.println("Error al conectar a la base de datos: " + e.toString());
+            System.err.println("Conexion exitosa");
         }
     }
     public Connection getCon() {
         return con;
+
     }
+
 }
